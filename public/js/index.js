e0e50de75f69a62451caa0dd6fc6e94f48f7bbb2
jhonataniberico <jhonatanibericom@gmail.com>
2018-02-15 18:35:23 -0500

    validaciones en el js

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,55 @@ function registrar() {
 	var cliente     = $('#cliente').val();
 	var productos   = $('#productos').val();
 	var attach      = $('#attach').val();
+
+	if(Nombre == null || Nombre == '') {
+		$('#Nombre').css('border-color','red');
+		return;
+	}
+	if(Apellido == null || Apellido == '') {
+		$('#Apellido').css('border-color','red');
+		return;
+	}
+	if(email == null || email == '') {
+		$('#email').css('border-color','red');
+		return;
+	}
+	if(email != re_email) {
+		msj('error', 'uno de los emails ingresados no coincide con el otro');
+		return;
+	}
+	if (!validateEmail(email)) {
+		$('#email').css('border-color','red');
+		return;
+	}
+	if (!validateEmail(re_email)) {
+		$('#correo').css('border-color','red');
+		return;
+	}
+	if(rol == null || rol == '') {
+		$('#rol').css('border-color','red');
+		return;
+	}
+	if(canal == null || canal == '') {
+		$('#canal').css('border-color','red');
+		return;
+	}
+	if(oportunidad == null || oportunidad == '') {
+		$('#oportunidad').css('border-color','red');
+		return;
+	}
+	if(cliente == null || cliente == '') {
+		$('#cliente').css('border-color','red');
+		return;
+	}
+	if(productos == null || productos == '') {
+		$('#productos').css('border-color','red');
+		return;
+	}
+	if(attach == null || attach == '') {
+		$('#attach').css('border-color','red');
+		return;
+	}
 	$.ajax({
 		data  : { Nombre 	  : Nombre,
 				  Apellido 	  : Apellido,
@@ -47,4 +96,37 @@ function limpiarCampos() {
 	var cliente     = $('#cliente').val(null);
 	var productos   = $('#productos').val(null);
 	var attach      = $('#attach').val(null);
+}
+
+function soloLetras(e) {
+    key 	   = e.keyCode || e.which;
+    tecla 	   = String.fromCharCode(key).toLowerCase();
+    letras     = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+    especiales = "8-37-39-46";
+
+    tecla_especial = false
+    for(var i in especiales){
+         if(key == especiales[i]){
+             tecla_especial = true;
+             break;
+         }
+     }
+     if(letras.indexOf(tecla)==-1 && !tecla_especial){
+         return false;
+     }
+ }
+ function valida(e) {
+    tecla = (document.all) ? e.keyCode : e.which;
+    //Tecla de retroceso para borrar, siempre la permite
+    if (tecla==8){
+        return true;
+    }
+    // Patron de entrada, en este caso solo acepta números
+    patron =/[0-9]/;
+    tecla_final = String.fromCharCode(tecla);
+    return patron.test(tecla_final);
+}
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
