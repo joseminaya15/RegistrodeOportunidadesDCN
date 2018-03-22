2e6587b90d27d3dce59304ef57410d18d5fa884a
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-03-22 17:42:42 -0500

    cambios

--- public/js/login.js
+++ public/js/login.js
@@ -85,3 +85,20 @@ function verificarDatos(e){
 		ingresar();
     }
 }
+function cerrarCesion(){
+  $.ajax({
+    url  : 'admin/cerrarCesion',
+    type : 'POST'
+  }).done(function(data){
+    try{
+        data = JSON.parse(data);
+        if(data.error == 0){
+          location.href = 'Login';
+        }else {
+          return;
+        }
+      }catch(err){
+        msj('error',err.message);
+      }
+  });
+}
