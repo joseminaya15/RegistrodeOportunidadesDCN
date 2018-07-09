d001f33ecaf0ada5d26e31180226c5a774190f46
Pedro Yturrizaga <blue2@MAC-BLUE2.local>
2018-07-09 12:20:09 -0500

    cambio contraseña

--- application/controllers/En.php
+++ application/controllers/En.php
@@ -74,8 +74,8 @@ class En extends CI_Controller {
                             'protocol'  => 'smtp',
                             'smtp_host' => 'smtpout.secureserver.net',
                             'smtp_port' => 3535,
-                            'smtp_user' => 'confirmaciones@merino.com.pe',
-                            'smtp_pass' => 'cFm$17Pe',
+                            'smtp_user' => 'info@marketinghpe.com',
+                            'smtp_pass' => 'hpEmSac$18',
                             'mailtype'  => 'html',
                             'charset'   => 'utf-8',
                             'newline'   => "\r\n"
